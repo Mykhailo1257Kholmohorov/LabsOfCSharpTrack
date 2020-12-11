d2fe47d38346f7da0f7aac7bc998279b14642e41
Mykhailo1257Kholmohorov <mykhailo1257@gmail.com>
2020-12-11 12:41:23 +0200

    fix

--- Lab1/Program.cs
+++ Lab1/Program.cs
@@ -9,6 +9,12 @@ namespace Lab1
 
             MyLinkedList<int> List = new MyLinkedList<int>(new []{11,22,33});
 
+            Console.WriteLine(List.Count);
+            Console.WriteLine("=======================");
+
+            Console.WriteLine(List.IsEmpty);
+            Console.WriteLine("=======================");
+
             List.Add(1);
             List.Add(2);
             List.Add(3);
@@ -20,6 +26,7 @@ namespace Lab1
                 Console.Write(item + " ");
             }
             Console.WriteLine();
+            Console.WriteLine("=======================");
 
             List.Remove(3);
             List.Remove(1);
@@ -29,6 +36,7 @@ namespace Lab1
                 Console.Write(item + " ");
             }
             Console.WriteLine();
+            Console.WriteLine("=======================");
 
             List.AppendHead(8);
             foreach (var item in List)
@@ -36,6 +44,7 @@ namespace Lab1
                 Console.Write(item + " ");
             }
             Console.WriteLine();
+            Console.WriteLine("=======================");
 
             List.AddAfter(4, 8);
             foreach (var item in List)
@@ -43,9 +52,24 @@ namespace Lab1
                 Console.Write(item + " ");
             }
             Console.WriteLine();
+            Console.WriteLine("=======================");
 
             if (List.Contains(4))
                 Console.WriteLine("contains 4");
+            Console.WriteLine("=======================");
+
+            int[] array = new int[3];
+
+            List.CopyTo(array,5);
+            foreach (var el in array)
+            {
+                Console.Write(el + " ");
+            }
+            Console.WriteLine();
+            Console.WriteLine("=======================");
+
+            List.Clear();
+            Console.WriteLine(List.IsEmpty);
 
         }
     }
---
+++ Lab1Tests/MyLinkedListTests.cs
@@ -0,0 +1,83 @@
+﻿using Xunit;
+using Lab1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Lab1.Tests
+{
+    public class MyLinkedListTests
+    {
+        [Fact()]
+        public void MyLinkedListTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void MyLinkedListTest1()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void MyLinkedListTest2()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AddTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void RemoveTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AppendHeadTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void AddAfterTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ClearTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ContainsTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void CopyToTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void GetEnumeratorTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+
+        [Fact()]
+        public void ToStringTest()
+        {
+            Assert.True(false, "This test needs an implementation");
+        }
+    }
+}
